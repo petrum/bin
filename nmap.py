04d3abf2b3f7b5438f8c442e78069e60c5aa1670
Petru Marginean <petru.marginean@gmail.com>
2019-06-24 09:05:32 -0400

    misc

--- nmap.py
+++ nmap.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python
 
 import pandas as pd
+import re
 
 class NMap:
     def __init__(self, d):
         self.descr = d
-    def get(self):
-        dDf = pd.read_csv(self.descr)
-        return dDf
+    def get(self, data = None):
+        d = pd.read_csv(self.descr)
+        df = pd.DataFrame(columns=['ip', 'dn', 'mac', 'company'])
+        if data != None:
+            with open(data) as fp:
+                line = fp.readline()
+                print line
+                n = re.search('Nmap scan report for (.*) \((.*)\)', line)
+                if n:
+                    df.loc[len(nmap)] = [n.group(2), n.group(1), None, None]
+                else:
+                    n = re.search('Nmap scan report for (.*)', line)
+                    if n:
+                        df.loc[len(nmap)] = [n.group(1), None, None, None]
+                m = re.search('MAC Address: (.*) \((.*)\)', line)
+                if m:
+                    last_index = len(nmap) - 1
+                    df.loc[last_index]['mac'] = m.group(1)
+                    df.loc[last_index]['company'] = m.group(2)
+
+        return df
 
 if __name__ == "__main__":
     n = NMap("/home/petrum/scripts/mac-addresses.csv")
-    print n.get()
+    print n.get("/home/petrum/scripts/nmap-sample1.txt")
 
 
 
