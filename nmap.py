a9a465989e2b2cf9851ecd7a3a2ca7747e4db486
Petru Marginean <petru.marginean@gmail.com>
2019-06-25 07:08:10 -0400

    misc

--- nmap.py
+++ nmap.py
@@ -13,23 +13,25 @@ class NMap:
         df = pd.DataFrame(columns=['ip', 'dn', 'mac', 'company'])
         if data != None:
             with open(data) as fp:
-                while True:
-                    line = fp.readline()
-                    #print(line)
-                    if not line:
-                        break
-                    n = re.search('Nmap scan report for (.*) \((.*)\)', line)
-                    if n:
-                        df.loc[len(df)] = [n.group(2), n.group(1), None, None]
-                    else:
-                        n = re.search('Nmap scan report for (.*)', line)
-                        if n:
-                            df.loc[len(df)] = [n.group(1), None, None, None]
-                    m = re.search('MAC Address: (.*) \((.*)\)', line)
-                    if m:
-                        last_index = len(df) - 1
-                        df.loc[last_index]['mac'] = m.group(1)
-                        df.loc[last_index]['company'] = m.group(2)
+                lines = fp.readlines()
+        else:
+            lines = sys.stdin.readlines()
+        for line in lines:
+            #print(line)
+            if not line:
+                break
+            n = re.search('Nmap scan report for (.*) \((.*)\)', line)
+            if n:
+                df.loc[len(df)] = [n.group(2), n.group(1), None, None]
+            else:
+                n = re.search('Nmap scan report for (.*)', line)
+                if n:
+                    df.loc[len(df)] = [n.group(1), None, None, None]
+            m = re.search('MAC Address: (.*) \((.*)\)', line)
+            if m:
+                last_index = len(df) - 1
+                df.loc[last_index]['mac'] = m.group(1)
+                df.loc[last_index]['company'] = m.group(2)
         res = pd.merge(df, d, how='left', left_on='mac', right_on='mac')
         res = res[res.mac.notnull()]
         res.set_index('ip', inplace=True)
