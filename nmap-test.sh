b1984c618e395c3c94fd61ff49d2abd2241303fd
Petru Marginean <petru.marginean@gmail.com>
2019-06-25 07:17:28 -0400

    misc

--- nmap-test.sh
+++ nmap-test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-./nmap.py /home/petrum/scripts/nmap-sample1.txt
-./nmap.py /home/petrum/scripts/nmap-sample2.txt
-./nmap.py
+./nmap.py /home/petrum/scripts/nmap-sample1.txt /home/petrum/scripts/nmap-sample2.txt
+sudo nmap -sP 192.168.1.1/24 | ./nmap.py
