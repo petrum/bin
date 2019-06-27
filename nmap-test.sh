9fd2be9b24518f3af7c91f824d008b66c03c9836
Petru Marginean <petru.marginean@gmail.com>
2019-06-27 19:35:38 -0400

    misc

--- nmap-test.sh
+++ nmap-test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./nmap.py -d /home/petrum/scripts/mac-addresses.csv /home/petrum/scripts/nmap-sample1.txt /home/petrum/scripts/nmap-sample2.txt
-./nmap.py /home/petrum/scripts/nmap-sample1.txt /home/petrum/scripts/nmap-sample2.txt
+./nmap.py -d /home/petrum/scripts/mac-addresses.csv /home/petrum/scripts/nmap-sample1.txt
+#./nmap.py -d /home/petrum/scripts/mac-addresses.csv /home/petrum/scripts/nmap-sample1.txt /home/petrum/scripts/nmap-sample2.txt
+#./nmap.py /home/petrum/scripts/nmap-sample1.txt /home/petrum/scripts/nmap-sample2.txt
 ./nmap.py -d /home/petrum/scripts/mac-addresses.csv
-./nmap.py
+#./nmap.py
