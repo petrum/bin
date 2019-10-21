6ac4fc5980778126465b7be005e0ab17b2c76310
petrum <petrum@users.noreply.github.com>
2019-10-21 13:16:26 -0400

    Update sleep.sh

--- sleep.sh
+++ sleep.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
 
+set -e
 UNTIL=$1
+NOW_SEC=$(date +%s)
 
 function doSleep {
-    NOW=$(date +"%Y-%m-%d %H:%M:%S")
-    echo "$NOW - sleeping $1 seconds until '$2'..." 1>&2
-    sleep $1
+    WAIT_SEC=$1
+    NOW=$(date -d @$NOW_SEC +"%Y-%m-%d %H:%M:%S")
+    END_SEC=$((NOW_SEC + WAIT_SEC))
+    END=$(date -d @$END_SEC +"%Y-%m-%d %H:%M:%S")
+    echo "$NOW - sleeping $1 seconds until '$END'..." 1>&2
+    sleep $WAIT_SEC
     #sleep 0
     NOW=$(date +"%Y-%m-%d %H:%M:%S")
     echo "$NOW - done sleeping" 1>&2
     exit 0
 }
 
-re='^[0-9]+$'
-
-if [[ $UNTIL =~ $re ]] ; then
-    doSleep $UNTIL done
-fi
-
-LEN=${#UNTIL}
-
-TODAY=$(date +"%Y-%m-%d")
-TOMORROW=$(date -d "1 day" +"%Y-%m-%d")
-NOW=$(date +%s)
-
-if [[ $LEN -eq 5 || $LEN -eq 8 ]]; then
-    D=$(date -d "$TODAY $UNTIL" +%s)
-    if [[ $D -lt $NOW ]]; then
-        D=$(date -d "$TOMORROW $UNTIL" +%s)
-    fi
-    SEC=$((D - NOW))
-    doSleep $SEC "$UNTIL"
-fi
-
-if [[ $LEN -eq 19 ]]; then
+if [[ $UNTIL =~ '^[0-9]+$' ]] ; then
+    echo "Specifying directly the wait time $UNTIL sec..."
+    WSEC=$UNTIL
+elif echo "$UNTIL" | grep -q '^20..-..-..'; then # starts with a date (like '2019-12-20')
+    echo "Specifying a datetime..."
     D=$(date -d "$UNTIL" +%s)
-    SEC=$((D - NOW))
-    if [[ $SEC -lt 0 ]]; then
+    WSEC=$((D - NOW_SEC))
+    if [[ $WSEC -lt 0 ]]; then
         echo "The date '$UNTIL' is in the past..." 1>&2
         exit -1
     fi
-    doSleep $SEC "$UNTIL"
+else
+    echo "Specify a time..."
+    D=$(date -d "$UNTIL" +%s)
+    if [[ $D -lt $NOW_SEC ]]; then
+        echo "Too late for today, going for next day..."
+        D=$(date -d "$UNTIL 1 day" +%s)
+    fi
+    WSEC=$((D - NOW_SEC))
 fi
 
-echo "I don't know what to do! What does '$UNTIL' mean?" 1>&2
-echo "(supported formats are: 'N' (sec), '09:30', '09:30:45', or '2019-10-17 09:30:24')" 1>&2
-exit -1
+doSleep $WSEC
