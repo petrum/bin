60f034d3c4928a433e64d90e3854888a9217f090
petrum <petrum@users.noreply.github.com>
2019-10-21 13:51:39 -0400

    Update sleep.sh

--- sleep.sh
+++ sleep.sh
@@ -6,6 +6,10 @@ NOW_SEC=$(date +%s)
 
 function doSleep {
     WAIT_SEC=$1
+    if [[ $WAIT_SEC -lt 0 ]]; then
+        echo "The wait time $WAIT_SEC is negative" 1>&2
+        exit -1
+    fi
     NOW=$(date -d @$NOW_SEC +"%Y-%m-%d %H:%M:%S")
     END_SEC=$((NOW_SEC + WAIT_SEC))
     END=$(date -d @$END_SEC +"%Y-%m-%d %H:%M:%S")
@@ -20,22 +24,18 @@ re='^[0-9]+$'
 if [[ $UNTIL =~ $re ]] ; then
     echo "Specifying directly the wait time $UNTIL sec..." 1>&2
     WSEC=$UNTIL
-elif echo "$UNTIL" | grep -q '^20..-..-..'; then # starts with a date (like '2019-12-20')
+elif echo "$UNTIL" | grep -q '^....-..-..'; then # starts with a date (like '2019-12-20')
     echo "Specifying a datetime..." 1>&2
-    D=$(date -d "$UNTIL" +%s)
-    WSEC=$((D - NOW_SEC))
-    if [[ $WSEC -lt 0 ]]; then
-        echo "The date '$UNTIL' is in the past..." 1>&2
-        exit -1
-    fi
+    WSEC=$(($(date -d "$UNTIL" +%s) - NOW_SEC))
 else
-    echo "Specify a time..." 1>&2
-    D=$(date -d "$UNTIL" +%s)
+    echo "Specifying a time..." 1>&2
+    WSEC=$(($(date -d "$UNTIL" +%s) - NOW_SEC))
     if [[ $D -lt $NOW_SEC ]]; then
         echo "Too late for today, going for next day..." 1>&2
-        D=$(date -d "$UNTIL 1 day" +%s)
+        WSEC=$(($(date -d "$UNTIL 1 day" +%s) - NOW_SEC))
     fi
-    WSEC=$((D - NOW_SEC))
 fi
+
+doSleep $WSEC
 
 doSleep $WSEC
